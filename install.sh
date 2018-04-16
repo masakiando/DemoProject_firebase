6b7918e1d907d457b125190476d873dfa594f30b
Your Name <you@example.com>
2018-04-16 16:26:36 +0700

    初コミット　shでフレーム作成　

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# install  DemoProject_firebase
+
+echo "WELCOME YOU WILL CREATE REDUX APP ..."
+echo ""
+
+echo '--> Create app tree...'
+
+mkdir css/
+mkdir images/
+mkdir js/
+touch index.html
+tree
